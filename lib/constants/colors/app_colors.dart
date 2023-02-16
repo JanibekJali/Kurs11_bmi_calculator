c0cf37f703406009414a74834fb92f99277f9aba
Ayanam <jumalievakani@gmail.com>
2023-02-16 20:30:24 +0300

    men colordu ozgortum

--- lib/constants/colors/app_colors.dart
+++ lib/constants/colors/app_colors.dart
@@ -3,4 +3,23 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color white = Colors.white;
   static Color secondaryColor = const Color(0xff211834);
+  static Color black = const Color(0xff000000);
+  static Color green = Colors.green;
+  static Color red = Colors.red;
+  static Color yelow =Colors.yellow;
+  static Color blue = Colors.blue; 
+  static Color brown = Colors.brown;
+  static Color indigo = Colors.indigo;
+  static Color color = Colors.lime;
+  static Color orange = Colors.orange;
+  static Color pink = Colors.pink;
+  static Color cyan  = Colors.cyan;
+  static Color purple  = Colors.purple;
+  static Color grey = Colors.grey;
+  static Color teal = Colors.teal;
+  static Color purpleAccent = Colors.purpleAccent;
+  static Color redAccent = Colors.redAccent;
+  static Color orangeAccent = Colors.orangeAccent;
 }
+
+
