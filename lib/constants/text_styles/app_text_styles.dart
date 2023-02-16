7b5a071e1e39ae0ca820cba5108bd0c271bf548d
Chopa092 <cholponbabaeva092@yahoo.com>
2023-02-16 23:58:13 +0600

    Text styles added

--- lib/constants/text_styles/app_text_styles.dart
+++ lib/constants/text_styles/app_text_styles.dart
@@ -10,4 +10,27 @@ class AppTextStyles {
     color: Colors.white.withOpacity(0.8),
     fontWeight: FontWeight.w500,
   );
+
+  static const TextStyle white40w800 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w800,
+  );
+
+  static const TextStyle white24w500 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle white80w500 = TextStyle(
+    fontSize: 80,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle white12w500 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle white18w500 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
 }
